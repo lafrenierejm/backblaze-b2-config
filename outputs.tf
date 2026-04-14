897db6383f01cfdc93ebb2c5c7677b21b31578a3
Joseph LaFreniere <git@lafreniere.xyz>
2026-04-14 02:43:51 -0500

    Output S3-compatible ID and secret

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,20 @@ output "application_key" {
   }
   sensitive = true
 }
+
+output "s3_access_key_id" {
+  description = "S3-compatible access key ID (B2 application key ID) per bucket."
+  value = {
+    for k, v in b2_application_key.key :
+    k => v.application_key_id
+  }
+}
+
+output "s3_secret_access_key" {
+  description = "S3-compatible secret access key (B2 application key) per bucket."
+  value = {
+    for k, v in b2_application_key.key :
+    k => v.application_key
+  }
+  sensitive = true
+}
